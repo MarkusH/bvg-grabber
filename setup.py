f751f585a8e34da2aa0c5f0f55fe9255f8d4d41d
Markus Holtermann <info@markusholtermann.eu>
2013-01-24 17:39:24 +0100

    Add tests to setup.py and exclude them during installation

--- setup.py
+++ setup.py
@@ -2,26 +2,33 @@ import codecs
 from os import path
 from setuptools import setup, find_packages
 
-read = lambda filepath: codecs.open(filepath, 'r', 'utf-8').read()
+BASEDIR = path.dirname(__file__)
+
+open = lambda filepath: codecs.open(filepath, 'r', 'utf-8')
+
+description = open(path.join(BASEDIR, 'README')).read()
+requirements = [line for line in open(path.join(BASEDIR, 'requirements.txt'))]
 
 setup(
-	name='bvg-grabber',
-	description='Display the upcoming departures of buses and subways in Berlin, Germany',
-	long_description=read(path.join(path.dirname(__file__), 'README')),
-	version='0.1a1',
-	url='https://github.com/Markush2010/bvg-grabber',
-	author='Christian Struck, Markus Holtermann',
-	author_email='',
-	license='BSD',
-	packages=find_packages(exclude=['example']),
-	include_package_data = True,
-	classifiers = [
-		'Development Status :: 2 - Pre-Alpha',
-		'Environment :: Console',
-		'Intended Audience :: Developers',
-		'License :: OSI Approved :: BSD License',
-		'Operating System :: OS Independent',
-		'Programming Language :: Python',
-		'Programming Language :: Python :: 3',
-	],
+    name='bvg-grabber',
+    description='Display the upcoming departures of buses and subways in Berlin, Germany',
+    long_description=description,
+    version='0.1a1',
+    url='https://github.com/Markush2010/bvg-grabber',
+    author='Christian Struck, Markus Holtermann',
+    author_email='',
+    license='BSD',
+    packages=find_packages(exclude=['example', 'tests']),
+    include_package_data=True,
+    install_requires=requirements,
+    test_suite="tests",
+    classifiers=[
+        'Development Status :: 2 - Pre-Alpha',
+        'Environment :: Console',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+    ],
 )
